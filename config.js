a25c14168963d6c81e8853a43700e25e99b74cb9
Cfaiboy <cfmaaiboy@gmail.com>
2026-04-30 18:45:14 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Apps Script Web App URL
-const API_BASE = "https://script.google.com/macros/s/AKfycby1NazwrP9bp_-1bTYr4u7tewZ39rbvrRl5vLMvuBPlNxhuqgzw2za_Fsv2EhDwJ5Sz/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbyEBNCpOFMUBgrohlsS6GgW2h7GBc_QcQOs27g3oQGnf1_cvLOCHcmoN5KnatV0qIBZ/exec";
